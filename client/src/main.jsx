e3e5be787212c0aa4ac41d98056837a95f25ef7b
IvanInandan <c.ivaninandan@gmail.com>
2025-05-30 14:26:01 -0700

    Stylize navbar using MantineUI

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,14 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
+// Import Mantine UI
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <MantineProvider theme={{ primaryColor: "red" }} defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
   </Router>
 );
