dd60540e76a7fb542e2d3911507f086b162dfac0
IvanInandan <c.ivaninandan@gmail.com>
2025-05-30 15:39:44 -0700

    Proxy front-end to send requests to port 3001 in vite config

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const middleware = require("./utils/middleware");
 
 const app = express();
 
-app.use(express.json());
+app.use(cors()); // Allows your port (server) to be reached by other ports (front-end, other users, etc)
+app.use(express.json()); // JSON Parser (parses request body to JSON before sending to routes)
+app.use(middleware.requestLogger); // Print request to console BEFORE sending
+
+// Implement token extractor below
+
+// Implement routers below
 
 // Middlewares executed after requests
 app.use(middleware.unknownEndpoint);
