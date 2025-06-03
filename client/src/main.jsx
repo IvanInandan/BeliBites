16b7e9d512f16a415402fae289eaefe41039136c
IvanInandan <c.ivaninandan@gmail.com>
2025-06-02 17:57:57 -0700

    Set up global state management using store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,27 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+// Set up react-router
 import { BrowserRouter as Router } from "react-router-dom";
+
+// Set up global state management
+import { Provider } from "react-redux";
+import store from "./store";
 
 // Import Mantine UI
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <MantineProvider theme={{ primaryColor: "red" }} defaultColorScheme="dark">
-      <App />
-    </MantineProvider>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <MantineProvider
+        theme={{ primaryColor: "red" }}
+        defaultColorScheme="dark"
+      >
+        <App />
+      </MantineProvider>
+    </Router>
+  </Provider>
 );
