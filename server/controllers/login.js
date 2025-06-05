92d79e7a32d23d70c57683cf74328cdec29c3fcf
IvanInandan <c.ivaninandan@gmail.com>
2025-06-04 20:56:52 -0700

    Install jsonwebtoken library for jwt token generation

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -1,8 +1,13 @@
 const loginRouter = require("express").Router();
+const User = require("../models/user");
+
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 loginRouter.post("/", async (request, response, next) => {
   try {
-    return response.status(201).json({ message: "hello" });
+    const { username, password } = request.body;
+    console.log(username, password);
   } catch (error) {
     next(error);
   }
