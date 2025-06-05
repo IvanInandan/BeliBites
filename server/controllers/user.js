78a1bb20de305790c8434eb8f69db26aa913d587
IvanInandan <c.ivaninandan@gmail.com>
2025-06-04 19:03:44 -0700

    Install mongoose for connecting to mongoDB

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,9 @@
 const userRouter = require("express").Router();
 
+userRouter.get("/", async (request, response, next) => {
+  return response.status(201).json({ message: "You made it!" });
+});
+
 userRouter.post("/", async (request, response, next) => {
   try {
     const user = request.body;
