28b2cb439a0024c56650d7f3645c78eef34a0908
IvanInandan <c.ivaninandan@gmail.com>
2025-06-04 20:06:15 -0700

    Establish database connection

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,3 +1,9 @@
 const PORT = process.env.PORT;
+const MONGODB_URI =
+  process.env.NODE_ENV === "production"
+    ? process.env.PROD_MONGODB_URI
+    : process.env.NODE_ENV === "development"
+    ? process.env.DEV_MONGODB_URI
+    : process.env.TEST_MONGODB_URI;
 
-module.exports = { PORT };
+module.exports = { MONGODB_URI, PORT };
