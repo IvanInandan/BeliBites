84be82913f2e1f36d8523129e9aaa0fd81e4cf11
IvanInandan <c.ivaninandan@gmail.com>
2025-06-08 21:36:42 -0700

    Initialize QueryClient in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Set up Tanstack Query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
 // Set up react-router
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -15,10 +19,12 @@ import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Router>
-      <MantineProvider defaultColorScheme="dark">
-        <App />
-      </MantineProvider>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <MantineProvider defaultColorScheme="dark">
+          <App />
+        </MantineProvider>
+      </Router>
+    </QueryClientProvider>
   </Provider>
 );
